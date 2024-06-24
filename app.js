e5bc5fd9e6a96166c74a211910582f4cf3b9f993
Ashis Shrestha <shrestha7.np@gmail.com>
2024-06-24 14:19:21 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hero-h1",{images: [""]});
+
+Shery.imageEffect(".destination-img", {
+    style: 2,
+    config: {onMouse: {value: 1}},
+    // debug: true,
+  });
